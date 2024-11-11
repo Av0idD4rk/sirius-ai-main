4fc930cc2bf49ed5a6556ac9bd08db79efd87b71
Av0idD4rk <mister57joy@gmail.com>
2024-11-11 20:48:11 +0300

    typo fix

--- backend_library/backend_library/settings.py
+++ backend_library/backend_library/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     },
     'postgres': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_DATABASE"),
+        'NAME': os.environ.get("POSTGRES_DB"),
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': 'db',
