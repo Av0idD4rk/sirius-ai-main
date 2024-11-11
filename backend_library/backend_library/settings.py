571203a6201b33c4e3476b67fed0b8ece1a9b234
Av0idD4rk <mister57joy@gmail.com>
2024-11-11 21:28:53 +0300

    new db

--- backend_library/backend_library/settings.py
+++ backend_library/backend_library/settings.py
@@ -86,10 +86,6 @@ WSGI_APPLICATION = 'backend_library.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
     'postgres': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("POSTGRES_DB"),
