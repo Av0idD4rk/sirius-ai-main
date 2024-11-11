5591e437b3098f44954b1b4e6ce49976bcee6ccb
Av0idD4rk <mister57joy@gmail.com>
2024-11-11 18:13:26 +0300

    postgres

--- backend_library/backend_library/settings.py
+++ backend_library/backend_library/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,6 +89,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
--- backend_library/entrypoint.sh
+++ backend_library/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+echo "Waiting for postgres..."
+while ! nc -z db 5432; do
+    sleep 0.1
+done
+echo "PostgreSQL started"
+
+
 # Exit immediately if a command exits with a non-zero status
 set -e
 echo "Making database migrations"
